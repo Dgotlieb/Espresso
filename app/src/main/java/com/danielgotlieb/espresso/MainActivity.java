3995353ce7e76c2a4ff93f9584d951edadebe69d
daniel.gotlieb <dgotlieb8@gmail.com>
2016-05-15 16:01:42 +0300

    comment

--- app/src/main/java/com/danielgotlieb/espresso/MainActivity.java
+++ app/src/main/java/com/danielgotlieb/espresso/MainActivity.java
@@ -2,13 +2,27 @@ package com.danielgotlieb.espresso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hello
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
